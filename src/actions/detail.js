bd574e86d14a4ac5448e85431ef944328c1c0190
Monali <monalidoshi9@gmail.com>
2021-11-06 23:16:12 +0530

    Bugfix fetchdetail backend url

--- src/actions/detail.js
+++ src/actions/detail.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { FETCH_DETAILS, FETCH_FAIL } from "./types";
+import { SERVER_URL } from "../constants";
 
 export const fetchdetail = (id) => async (dispatch) => {
   try {
-    const res = await axios.get("http://localhost:4000/auth/detail/" + id);
+    const res = await axios.get(`${SERVER_URL}/auth/detail/` + id);
     dispatch({
       type: FETCH_DETAILS,
       payload: res.data.data,
